e5f4d657449ea5bb79d06ba1a7715f9efd6d8a28
Wired-Dollar <wired.1.dollar@gmail.com>
2019-03-30 11:52:23 -0500

    adding second.js

---
+++ second.js
@@ -0,0 +1,12 @@
+
+$("#add-employee-btn").click(function (x) {
+    var employeeName = $("#employee-name-input").val();
+    var role = $("#role-input").val();
+    var startDate = $("#start-input").val();
+    var monthlyRate = $("#rate-input").val();
+    console.log(employeeName)
+    console.log(role)
+    console.log(startDate)
+    console.log(monthlyRate)
+
+})
